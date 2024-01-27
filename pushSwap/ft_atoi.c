d2c953427a40cf66a94c05a01cdac1886cb8db2a
Arif Mohammed <arimoham@lab1r5s14.42abudhabi.ae>
2024-01-27 14:07:13 +0400

    atoi fixed

--- pushSwap/ft_atoi.c
+++ pushSwap/ft_atoi.c
@@ -6,61 +6,36 @@
 /*   By: arimoham <arimoham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:48:16 by arimoham          #+#    #+#             */
-/*   Updated: 2024/01/07 13:36:36 by arimoham         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:06:58 by arimoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	check_llong(int sign, unsigned long long j)
-{
-	if (sign == -1 && j > LLONG_MAX)
-		return (0);
-	if (sign == 1 && j > LLONG_MAX)
-		return (-1);
-	return (1);
-}
-
-int	ft_isspace(int c)
-{
-	if ((c == ' ' || c == '\t' || c == '\v'
-			|| c == '\n' || c == '\f' || c == '\r'))
-		return (1);
-	return (0);
-}
-
-int	check_sign(const char *str, int sign, size_t *i)
-{
-	if ((str[(*i)] == '-' || str[(*i)] == '+') && (str[(*i) + 1]
-			&& str[(*i) + 1] >= '0' && str[(*i) + 1] <= '9'))
-	{
-		if (str[(*i)++] == '-')
-			sign = sign * -1;
-	}
-	return (sign);
-}
-
 int	ft_atoi(const char *str)
 {
-	unsigned long long	j;
-	size_t				i;
-	int					sign;
+	long long	num;
+	int			n;
 
-	i = 0;
-	j = 0;
-	sign = 1;
-	while (str[i] && ft_isspace(str[i]))
-		i++;
-	sign = check_sign(str, sign, &i);
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	num = 0;
+	n = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if ((*str == '+' || *str == '-') && (*(str + 1)
+			&& *(str + 1) >= '0' && *(str + 1) <= '9'))
 	{
-		if (check_llong(sign, j) != 1)
-			return (check_llong(sign, j));
-		j = (j * 10) + (str[i++] - '0');
+		if (*str == '-')
+			n = -1;
+		str++;
 	}
-	if (check_llong(sign, j) != 1)
-		return (check_llong(sign, j));
-	if ((str[i] && str[i] < '0') || (str[i] && str[i] > '9'))
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + *str - '0';
+		if ((n == -1 && num * n < INT_MIN) || (n == 1 && num > INT_MAX))
+			return (0);
+		str++;
+	}
+	if ((*str && *str < '0') || (*str && *str > '9'))
 		return (0);
-	return (sign * j);
+	return (num * n);
 }
